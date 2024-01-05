a76c7f5aceff5ef306200cd33acb1a2e1ede6a4c
cspark8172 <cspark8172@gmail.com>
2024-01-05 10:00:02 +0900

    update
    - app.js
      : code_interpreter -> retrieval
    - pricing info

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ import morgan from "morgan";
 
 dotenv.config();
 
+// 환경변수로 node에서 허가되지 않은 인증TLS통신을 거부하지 않겠다고 설정
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const app = express();
 app.use(bodyParser.json());
 app.use(morgan("combined"));
@@ -26,8 +29,8 @@ async function createAssistant() {
 
   const assistantResponse = await openai.beta.assistants.create({
     name: "Professional Teacher given STT", // adjust name as per requirement
-    instructions: "You are a professional Teacher based on STT.Always Behave Like Professional and friendly teacher.",
-    tools: [{ type: "code_interpreter" }], // adjust tools as per requirement
+    instructions: "You are a professional Teacher based on STT.Always Behave Like Professional and friendly teacher. Do not answer the question which is not referenced in a given file.",
+    tools: [{ type: "retrieval" }], // adjust tools as per requirement
     model: "gpt-4-1106-preview", // or any other GPT-3.5 or GPT-4 model
     file_ids: [file.id]
   });
@@ -74,11 +77,12 @@ app.post("/chat", async (req, res) => {
     // Display the Assistant's Response
     const messagesResponse = await openai.beta.threads.messages.list(threadId);
     const assistantResponses = messagesResponse.data.filter(msg => msg.role === 'assistant');
+
     const response = assistantResponses.map(msg => 
       msg.content
         .filter(contentItem => contentItem.type === 'text')
         .map(textContent => textContent.text.value)
-        .join('\n')
+        // .join('\n')
     ).join('\n');
 
     res.json({ response });
